3e3529f02aff1962269886632b4d172717aa5c26
Dimosthenis Kanellopoulos <dimoskan12@gmail.com>
2022-05-10 14:27:39 +0000

    Added a function that gets the json file in jquery

--- putri.js
+++ putri.js
@@ -7,4 +7,8 @@ client.once('ready', () => {
 	console.log('Good news everyone! The bot is working once again!');
 });
 
+$.getJSON('http://armory.warmane.com/api/guild/Blood+Legion/Lordaeron/summary', function(data) {
+    // JSON result in `data` variable
+	console.log(data)
+});
 
